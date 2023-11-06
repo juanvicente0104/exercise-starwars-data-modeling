1c41943f92a10703ac2b55648492f843c2bbb727
Juan Guerrero <juanvicente0104@gmail.com>
2023-11-06 04:38:30 +0000

    first version of the UML diagram

--- src/models.py
+++ src/models.py
@@ -7,26 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(40),nullable=False, unique=True)
+    email = Column(String(80),nullable=False, unique=True)
+    password = Column(String(100),nullable=False)
+    favorite = relationship('Favorite',uselist=True,backref="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Character(Base):
+    __tablename__ = "character"
+    id = Column(Integer,primary_key=True)
+    name = Column(String(40),nullable=False, unique=True)
+    height = Column(Integer,nullable=False, unique=True)
+    mass = Column(Integer,nullable=False, unique=True)
+    gender = Column(String(10),nullable=False)
+    favorite = relationship('Favorite',uselist=True,backref="character")
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer,primary_key=True)
+    name = Column(String(40),nullable=False,unique=True)
+    climate = Column(String(20),nullable=False)
+    terrain = Column(String(20),nullable=False)
+    population = Column(Integer,nullable=False)
+    diameter = Column(Integer,nullable=False)
+    favorite = relationship('Favorite',uselist=True,backref="planet")
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+    id = Column(Integer,primary_key=True)
+
+    user_id = Column(Integer(),ForeignKey("user.id"))
+    character_id = Column(Integer(),ForeignKey("character.id"))
+    planet_id = Column(Integer(),ForeignKey("planet.id"))
+        
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
